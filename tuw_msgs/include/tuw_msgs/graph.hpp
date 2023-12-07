5cf921564bda8d2627ea5ad409b9612f644624f7
Markus Bader <markus.bader@tuwien.ac.at>
2023-12-07 08:54:30 +0100

    x

--- tuw_msgs/include/tuw_msgs/graph.hpp
+++ tuw_msgs/include/tuw_msgs/graph.hpp
@@ -6,37 +6,11 @@
 
 namespace tuw_msgs
 {
-  using Idx = int64_t;
-  class Node : public tuw_graph_msgs::msg::Node
-  {
-  public:
-    Node();
-    Node(Idx id, const Pose &pose);
-    void set(Idx id, const Pose &pose);
-    Pose &get_pose();
-    const Pose &get_pose() const;
-    bool operator==(const Node& rhs) const;
-  }; // class Node
-
-
-  class Edge : public tuw_graph_msgs::msg::Edge
-  {
-  public:
-    Edge();
-    bool operator==(const Edge& rhs) const;
-  }; // class Edge
-
-
   class Graph : public tuw_graph_msgs::msg::Graph
   {
   public:
     Graph();
     bool operator==(const Graph& rhs) const;
   }; // class Graph
-  
-    std::string &encode(tuw_graph_msgs::msg::Node &src, std::string &des);
-    size_t decode(tuw_graph_msgs::msg::Node &des, std::string &line, size_t pos = 0);
-    std::string &encode(tuw_graph_msgs::msg::Edge &src, std::string &des);
-    size_t decode(tuw_graph_msgs::msg::Edge &des, std::string &line, size_t pos = 0);
 };
 #endif // TUW_MSGS__GRAPH_HPP_
