b203e4fe7099df722d137b6439d98b16620263c4
Markus Bader <markus.bader@mx-robotics.com>
2024-03-23 19:57:34 +0100

    code formatierung fixed

--- tuw_object_map_msgs/include/tuw_object_map_msgs/geo_json_geometry_json.hpp
+++ tuw_object_map_msgs/include/tuw_object_map_msgs/geo_json_geometry_json.hpp
@@ -6,38 +6,34 @@
 
 namespace tuw_json
 {
-    inline tuw_object_map_msgs::msg::GeoJSONGeometry &fromJson(
-        const Json::Value &json, tuw_object_map_msgs::msg::GeoJSONGeometry &des)
-    {
-        des.type = json.get("type", "").asCString();
+inline tuw_object_map_msgs::msg::GeoJSONGeometry & fromJson(
+  const Json::Value & json, tuw_object_map_msgs::msg::GeoJSONGeometry & des)
+{
+  des.type = json.get("type", "").asCString();
 
-        if (json["coordinates"][0].isArray())
-        {
-            const Json::Value &coordinatesArray = json["coordinates"];
-            for (auto &coordinateJson : coordinatesArray)
-            {
-                tuw_object_map_msgs::msg::GeoPoint point;
+  if (json["coordinates"][0].isArray()) {
+    const Json::Value & coordinatesArray = json["coordinates"];
+    for (auto & coordinateJson : coordinatesArray) {
+      tuw_object_map_msgs::msg::GeoPoint point;
 
-                point.longitude = coordinateJson[0].asDouble();
-                point.latitude = coordinateJson[1].asDouble();
-                // point.altitude = coordinateJson[2].asDouble(); TODO: Add altitude to GeoPoint
+      point.longitude = coordinateJson[0].asDouble();
+      point.latitude = coordinateJson[1].asDouble();
+      // point.altitude = coordinateJson[2].asDouble(); TODO: Add altitude to GeoPoint
 
-                des.coordinates.push_back(std::move(point));
-            }
-        }
-        else
-        {
-            const Json::Value &coordinates = json["coordinates"];
-
-            tuw_object_map_msgs::msg::GeoPoint point;
-
-            point.longitude = coordinates[0].asDouble();
-            point.latitude = coordinates[1].asDouble();
-            // point.altitude = coordinateJson[2].asDouble(); TODO: Add altitude to GeoPoint
-
-            des.coordinates.push_back(std::move(point));
-        }
-
-        return des;
+      des.coordinates.push_back(std::move(point));
     }
-} // namespace tuw_json
+  } else {
+    const Json::Value & coordinates = json["coordinates"];
+
+    tuw_object_map_msgs::msg::GeoPoint point;
+
+    point.longitude = coordinates[0].asDouble();
+    point.latitude = coordinates[1].asDouble();
+    // point.altitude = coordinateJson[2].asDouble(); TODO: Add altitude to GeoPoint
+
+    des.coordinates.push_back(std::move(point));
+  }
+
+  return des;
+}
+}  // namespace tuw_json
