353b24b6a0b6ffba8f479f48a22b979419c72836
Markus Bader <markus.bader@tuwien.ac.at>
2024-11-25 07:38:40 +0000

    commends reformated

--- tuw_std_msgs/include/tuw_std_msgs/parameter_array.hpp
+++ tuw_std_msgs/include/tuw_std_msgs/parameter_array.hpp
@@ -74,13 +74,13 @@ struct ParameterArray : public tuw_std_msgs::msg::ParameterArray
     }
   }
 
- /**
-   * searches in the array for a parameter name
-   * @param name name of the parameter
-   * @param data value to add
-   * @param precision precision used
-   * @return false if it exited and it was set, true if was newly added
-   */
+  /**
+    * searches in the array for a parameter name
+    * @param name name of the parameter
+    * @param data value to add
+    * @param precision precision used
+    * @return false if it exited and it was set, true if was newly added
+    */
   template<typename T>
   bool add(const std::string & name, const T & data, int precision)
   {
